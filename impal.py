9bb70cabcd63c5b0ab99bb6ce4e7e51abe23ca8e
rrahmattullah <54544422+rrahmattullah@users.noreply.github.com>
2019-09-18 12:49:02 +0700

    Add files via upload

---
+++ impal.py
@@ -0,0 +1,13 @@
+def pilihvilla():
+	print("Pilih Villa : \n"," 1. Villa Lembang Asri\n","2. Villa Istana Bunga\n","3. Villa Merah\n","4. Villa Orange\n","5. Villa Kenangan")
+	pilih=int(input('Pilih villa anda: '))
+	if pilih==1:
+		print("Villa Lembang Asri, Jln. Lembang no. 5, Rp. 3500000")
+	elif pilih==2:
+		print("Villa Istana Bunga, Jln. Mawar no. 11, Rp. 3000000")
+	elif pilih==3:
+		print("Villa Merah Jln. Merah no. 12, Rp. 2750000")
+	elif pilih==4:
+		print("Villa Orange Jln. Orange no. 7, Rp. 2500000")
+	elif pilih==5:
+		print("Villa Kenangan Jln. Kenangan no. 3, Rp. 5000000")
